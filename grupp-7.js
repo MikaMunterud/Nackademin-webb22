6205d8219441a5358b48bd564da1ce85c01c704a
Mika Munterud <mika.munterud@outlook.com>
2022-10-03 15:02:32 +0200

    update

--- grupp-7.js
+++ grupp-7.js
@@ -1,3 +1,19 @@
 //Hej
 //Vad gör ni? 
-//Helvetes jävla pisskit
+//lägga in fetch här
+const elForm = document.querySelector("#nameForm");
+
+function getMember(event){
+
+fetch("members.json").then(response => response.json()).then(data => {
+    data.map(person=> console.log(person));
+
+})
+
+//använd filter för att få ut rätt person.
+//i html skapa en ruta där användaren kan skriva in vilket namn man vill få ut
+//se till så att det inskrivna namnet och det hämtade namet har .toLowerCase så att rätt namn kan hittas
+
+event.preventDefault();
+}
+elForm.addEventListener("submit", getMember)
