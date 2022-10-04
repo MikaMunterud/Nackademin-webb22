dbe097c49b5e47c830f56118259e31d66cf2ce6a
Mika Munterud <mika.munterud@outlook.com>
2022-10-04 09:49:08 +0200

    update

--- grupp-7.js
+++ grupp-7.js
@@ -1,19 +1,46 @@
-//Hej
-//Vad gör ni? 
-//lägga in fetch här
 const elForm = document.querySelector("#nameForm");
 
-function getMember(event){
+function getMember(event) {
+  document.querySelector("#output").innerHTML = "";
+  const value = document.querySelector("#name").value;
 
-fetch("members.json").then(response => response.json()).then(data => {
-    data.map(person=> console.log(person));
+  fetch("members.json")
+    .then((response) => response.json())
+    .then((data) => {
+      //**Print all information of searched person
+      // const result = data.filter(
+      //   (person) => person.firstname.toLowerCase() === value.trim().toLowerCase()
+      // );
+      // if (result.length > 0) {
+      //   // console.log(Object.keys(result[0]));
+      //   Object.keys(result[0]).map((k) => {
+      //     document.querySelector(
+      //       "#output"
+      //     ).innerHTML += `<p>${k}: </p>${result[0][k]}<br>`;
+      //   });
+      // }
+      //** */  print all person by key{firstname,lastname,..}
+      // data.map((person) => {
+      //   document.querySelector(
+      //     "#output"
+      //   ).innerHTML += `${value}: ${person[value]}<br>`;
+      // });
+      //result.map((person) => console.log(person));
 
-})
+      const result = data.filter(
+        (d) => d.firstname.toLowerCase() === value.trim().toLowerCase()
+      );
+      if (result.length > 0) {
+        document.querySelector(
+          "#output"
+        ).innerHTML = `<p>**information**</p>${result[0].firstname} ${result[0].lastname}<br>`;
+      }
+    });
 
-//använd filter för att få ut rätt person.
-//i html skapa en ruta där användaren kan skriva in vilket namn man vill få ut
-//se till så att det inskrivna namnet och det hämtade namet har .toLowerCase så att rätt namn kan hittas
+  //använd filter för att få ut rätt person.
+  //i html skapa en ruta där användaren kan skriva in vilket namn man vill få ut
+  //se till så att det inskrivna namnet och det hämtade namet har .toLowerCase så att rätt namn kan hittas
 
-event.preventDefault();
+  event.preventDefault();
 }
-elForm.addEventListener("submit", getMember)
+elForm.addEventListener("submit", getMember);
