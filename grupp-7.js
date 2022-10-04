8056be51525c7ceb8d41d9214cc184ddc845a6ed
Mika Munterud <mika.munterud@outlook.com>
2022-10-04 11:47:02 +0200

    update

--- grupp-7.js
+++ grupp-7.js
@@ -8,17 +8,22 @@ function getMember(event) {
     .then((response) => response.json())
     .then((data) => {
       //**Print all information of searched person
-      // const result = data.filter(
-      //   (person) => person.firstname.toLowerCase() === value.trim().toLowerCase()
-      // );
-      // if (result.length > 0) {
-      //   // console.log(Object.keys(result[0]));
-      //   Object.keys(result[0]).map((k) => {
-      //     document.querySelector(
-      //       "#output"
-      //     ).innerHTML += `<p>${k}: </p>${result[0][k]}<br>`;
-      //   });
-      // }
+      const result = data.filter(
+        (person) =>
+          person.firstname.toLowerCase() === value.trim().toLowerCase()
+      );
+      if (result.length > 0) {
+        // console.log(Object.keys(result[0]));
+        Object.keys(result[0]).map((k) => {
+          if (k === "image") {
+            document.querySelector("#image").src = result[0].image;
+          } else {
+            document.querySelector(
+              "#output"
+            ).innerHTML += `<span>${k}: </span>${result[0][k]}<br>`;
+          }
+        });
+      }
       //** */  print all person by key{firstname,lastname,..}
       // data.map((person) => {
       //   document.querySelector(
@@ -27,14 +32,15 @@ function getMember(event) {
       // });
       //result.map((person) => console.log(person));
 
-      const result = data.filter(
-        (d) => d.firstname.toLowerCase() === value.trim().toLowerCase()
-      );
-      if (result.length > 0) {
-        document.querySelector(
-          "#output"
-        ).innerHTML = `<p>**information**</p>${result[0].firstname} ${result[0].lastname}<br>`;
-      }
+      //   const result = data.filter(
+      //     (d) => d.firstname.toLowerCase() === value.trim().toLowerCase()
+      //   );
+      //   if (result.length > 0) {
+      //     document.querySelector(
+      //       "#output"
+      //     ).innerHTML = `<p>**information**</p>${result[0].firstname} ${result[0].lastname}<br>`;
+      //     document.querySelector("#image").src = result[0].image;
+      //   }
     });
 
   //använd filter för att få ut rätt person.
